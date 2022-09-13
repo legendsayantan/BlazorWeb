716cccd6c84354ebf939d3233cc7df0e5450e3fd
legendsayantan <legendsayantanpaul@gmail.com>
2022-09-13 22:57:07 +0530

    Add project files.

---
+++ BlazorWeb/Program.cs
@@ -0,0 +1,24 @@
+using BlazorWeb;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Radzen;
+using MatBlazor;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        var builder = WebAssemblyHostBuilder.CreateDefault(args);
+        builder.RootComponents.Add<App>("#app");
+        builder.RootComponents.Add<HeadOutlet>("head::after");
+
+        builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+
+        builder.Services.AddScoped<DialogService>();
+        builder.Services.AddScoped<NotificationService>();
+        builder.Services.AddScoped<TooltipService>();
+        builder.Services.AddScoped<ContextMenuService>();
+        await builder.Build().RunAsync();
+    }
+}
---
+++ BlazorWeb/Project.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorWeb
+{
+    public class Project
+    {
+        public string? name { get; set; }
+        public string? desc { get; set; }
+        public string? path { get; set; }
+    }
+}
