314582e00b34e10c9496e72951dac7230a48623f
legendsayantan <legendsayantanpaul@gmail.com>
2022-09-16 23:44:02 +0530

    Addded Xcrypt Demo mode

--- BlazorWeb/Program.cs
+++ BlazorWeb/Program.cs
@@ -2,19 +2,21 @@ using BlazorWeb;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Radzen;
+using Blazor.SubtleCrypto;
+using FileDownloader;
 using MatBlazor;
 
 internal class Program
 {
+    static WebAssemblyHostBuilder builder;
     private static async Task Main(string[] args)
     {
-        var builder = WebAssemblyHostBuilder.CreateDefault(args);
+        builder = WebAssemblyHostBuilder.CreateDefault(args);
         builder.RootComponents.Add<App>("#app");
         builder.RootComponents.Add<HeadOutlet>("head::after");
-
         builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
-
+        builder.Services.AddFileDownloder();
+        builder.Services.AddSubtleCrypto(opt => opt.Key = "samplexcryptdemo");
         builder.Services.AddScoped<DialogService>();
         builder.Services.AddScoped<NotificationService>();
         builder.Services.AddScoped<TooltipService>();
