a3be5dc210a4f8674b9ea1e87d03e7c8fe3aceba
legendsayantan <legendsayantanpaul@gmail.com>
2023-01-19 17:34:50 +0530

    improved the app list ui

--- BlazorWeb/Project.cs
+++ BlazorWeb/Project.cs
@@ -6,7 +6,7 @@
         public string? desc { get; set; }
         public string? btn1 { get; set; }
         public string? btn2 { get; set; }
-
+        public string? path { get; set; }
         public string? url1 { get; set; }
         public string? url2 { get; set; }
 
