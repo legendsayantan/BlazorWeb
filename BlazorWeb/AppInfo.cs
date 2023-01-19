a3a5935465530c2a2cf5f409d868d0c8fc44f4d9
legendsayantan <legendsayantanpaul@gmail.com>
2023-01-19 18:37:15 +0530

    added capability to uninstall apps

--- BlazorWeb/AppInfo.cs
+++ BlazorWeb/AppInfo.cs
@@ -3,8 +3,8 @@
     public class AppInfo
     {
         public string id { get; set; }
-        public string description { get; set; } = "unknown";
-        public string removal { get; set; } = "unknown";
+        public string description { get; set; } = "No description available";
+        public string removal { get; set; } = "Unknown";
         public AppInfo() { }
     }
 }
