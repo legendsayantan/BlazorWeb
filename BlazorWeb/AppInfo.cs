aad5f293a2f53837b8a86a5edb3b289ae4c7cf58
legendsayantan <legendsayantanpaul@gmail.com>
2023-01-19 16:01:59 +0530

    deploy test

---
+++ BlazorWeb/AppInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorWeb
+{
+    public class AppInfo
+    {
+        public string id { get; set; }
+        public string description { get; set; } = "unknown";
+        public string removal { get; set; } = "unknown";
+        public AppInfo() { }
+    }
+}
