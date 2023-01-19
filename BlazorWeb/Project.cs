209f14b8e4caf75872fb1d2fec97e3ff11344514
legendsayantan <legendsayantanpaul@gmail.com>
2023-01-19 17:00:30 +0530

    deployed test

--- BlazorWeb/Project.cs
+++ BlazorWeb/Project.cs
@@ -4,6 +4,11 @@
     {
         public string? name { get; set; }
         public string? desc { get; set; }
-        public string? path { get; set; }
+        public string? btn1 { get; set; }
+        public string? btn2 { get; set; }
+
+        public string? url1 { get; set; }
+        public string? url2 { get; set; }
+
     }
 }
