00d2ad4bdd59b23369b972ecf9448fce7ca127a3
legendsayantan <legendsayantanpaul@gmail.com>
2023-01-22 18:13:50 +0530

    automatic sitemap generation

---
+++ BlazorWeb/sitemap.sh
@@ -0,0 +1,12 @@
+filename='wwwroot/sitemap.xml'
+page='legendsayantan.github.io'
+curDate=$(date +'%Y-%m-%d')
+if [ -f "$filename" ] ; then
+    rm "$filename"
+fi
+IFS=$'\n' read -r -d '' -a allPaths < <( jq '.[] | .path' -r wwwroot/data/projects.json && printf '\0' )
+echo '<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' >>$filename
+for str in ${allPaths[@]}; do
+  echo "<url><loc>https://${page}${str}</loc><lastmod>${curDate}</lastmod></url>" >> $filename
+done
+echo '</urlset>' >> $filename
